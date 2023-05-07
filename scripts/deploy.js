f76d7e28fa1e08f022b6137ae8db239833144bb7
aleeeexcool <ramzhev.sasha@outlook.com>
2023-05-07 21:27:20 +0300

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,15 @@ const hre = require("hardhat");
 
 async function main() {
   const NFTWrapper = await ethers.getContractFactory('NFTWrapper');
-
   const wrapper = await NFTWrapper.deploy();
   await wrapper.deployed();
+
+  const MyToken = await ethers.getContractFactory('MyToken');
+  const token = await MyToken.deploy(10000);
+  await token.deployed();
+
   console.log('NFTWrapper deployed to:', wrapper.address);
+  console.log('MyToken deployed to:', token.address);
 }
 
 main().catch((error) => {
